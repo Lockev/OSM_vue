8f4a0fcc352ffc3580af7d345a917be221180fed
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2019-12-04 11:00:17 +0100

    Changes for teaser

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,6 +1,13 @@
 <template>
-  <div id="fdp">
-    <l-map :zoom.sync="zoom" :options="mapOptions" :center="center" :bounds="bounds" :min-zoom="minZoom" :max-zoom="maxZoom">
+  <div id="mymap">
+    <l-map
+      :zoom.sync="zoom"
+      :options="mapOptions"
+      :center="center"
+      :bounds="bounds"
+      :min-zoom="minZoom"
+      :max-zoom="maxZoom"
+    >
       <l-tile-layer
         v-for="tileProvider in tileProviders"
         :key="tileProvider.name"
@@ -27,9 +34,21 @@
         <l-tooltip :content="marker.tooltip" />
       </l-marker>
       <l-layer-group layer-type="overlay" name="Layer polyline">
-        <l-polyline v-for="item in polylines" :key="item.id" :lat-lngs="item.points" :visible="item.visible" @click="alert(item)" />
+        <l-polyline
+          v-for="item in polylines"
+          :key="item.id"
+          :lat-lngs="item.points"
+          :visible="item.visible"
+          @click="alert(item)"
+        />
       </l-layer-group>
-      <l-layer-group v-for="item in stuff" :key="item.id" :visible="item.visible" layer-type="overlay" name="Layer 1">
+      <l-layer-group
+        v-for="item in stuff"
+        :key="item.id"
+        :visible="item.visible"
+        layer-type="overlay"
+        name="Layer 1"
+      >
         <l-layer-group :visible="item.markersVisible">
           <l-marker
             v-for="marker in item.markers"
@@ -40,7 +59,11 @@
             @click="alert(marker)"
           />
         </l-layer-group>
-        <l-polyline :lat-lngs="item.polyline.points" :visible="item.polyline.visible" @click="alert(item.polyline)" />
+        <l-polyline
+          :lat-lngs="item.polyline.points"
+          :visible="item.polyline.visible"
+          @click="alert(item.polyline)"
+        />
       </l-layer-group>
     </l-map>
   </div>
@@ -48,70 +71,83 @@
 
 <script>
 import { icon, latLngBounds } from "leaflet";
-import { LMap, LTileLayer, LMarker, LPolyline, LLayerGroup, LTooltip, LPopup, LControlZoom, LControlAttribution, LControlScale } from "vue2-leaflet";
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LPolyline,
+  LLayerGroup,
+  LTooltip,
+  LPopup,
+  LControlZoom,
+  LControlAttribution,
+  LControlScale
+} from "vue2-leaflet";
 
 const markers1 = [
-  {
-    position: { lng: -1.219482, lat: 47.41322 },
-    visible: true,
-    draggable: true
-  },
-  { position: { lng: -1.571045, lat: 47.457809 } },
-  { position: { lng: -1.560059, lat: 47.739323 } },
-  { position: { lng: -0.922852, lat: 47.886881 } },
-  { position: { lng: -0.769043, lat: 48.231991 } },
-  { position: { lng: 0.395508, lat: 48.268569 } },
-  { position: { lng: 0.604248, lat: 48.026672 } },
-  { position: { lng: 1.2854, lat: 47.982568 } },
-  { position: { lng: 1.318359, lat: 47.894248 } },
-  { position: { lng: 1.373291, lat: 47.879513 } },
-  { position: { lng: 1.384277, lat: 47.798397 } },
-  { position: { lng: 1.329346, lat: 47.754098 } },
-  { position: { lng: 1.329346, lat: 47.680183 } },
-  { position: { lng: 0.999756, lat: 47.635784 } },
-  { position: { lng: 0.86792, lat: 47.820532 } },
-  { position: { lng: 0.571289, lat: 47.820532 } },
-  { position: { lng: 0.439453, lat: 47.717154 } },
-  { position: { lng: 0.439453, lat: 47.61357 } },
-  { position: { lng: -0.571289, lat: 47.487513 } },
-  { position: { lng: -0.615234, lat: 47.680183 } },
-  { position: { lng: -0.812988, lat: 47.724545 } },
-  { position: { lng: -1.054688, lat: 47.680183 } },
-  { position: { lng: -1.219482, lat: 47.41322 } }
+  // {
+  //   position: { lng: -1.219482, lat: 47.41322 },
+  //   visible: true,
+  //   draggable: true
+  // },
+  // { position: { lng: -1.571045, lat: 47.457809 } },
+  // { position: { lng: -1.560059, lat: 47.739323 } },
+  // { position: { lng: -0.922852, lat: 47.886881 } },
+  // { position: { lng: -0.769043, lat: 48.231991 } },
+  // { position: { lng: 0.395508, lat: 48.268569 } },
+  // { position: { lng: 0.604248, lat: 48.026672 } },
+  // { position: { lng: 1.2854, lat: 47.982568 } },
+  // { position: { lng: 1.318359, lat: 47.894248 } },
+  // { position: { lng: 1.373291, lat: 47.879513 } },
+  // { position: { lng: 1.384277, lat: 47.798397 } },
+  // { position: { lng: 1.329346, lat: 47.754098 } },
+  // { position: { lng: 1.329346, lat: 47.680183 } },
+  // { position: { lng: 0.999756, lat: 47.635784 } },
+  // { position: { lng: 0.86792, lat: 47.820532 } },
+  // { position: { lng: 0.571289, lat: 47.820532 } },
+  // { position: { lng: 0.439453, lat: 47.717154 } },
+  // { position: { lng: 0.439453, lat: 47.61357 } },
+  // { position: { lng: -0.571289, lat: 47.487513 } },
+  // { position: { lng: -0.615234, lat: 47.680183 } },
+  // { position: { lng: -0.812988, lat: 47.724545 } },
+  // { position: { lng: -1.054688, lat: 47.680183 } },
+  // { position: { lng: -1.219482, lat: 47.41322 } }
 ];
 
-const poly1 = [
-  { lng: -1.219482, lat: 47.41322 },
-  { lng: -1.571045, lat: 47.457809 },
-  { lng: -1.560059, lat: 47.739323 },
-  { lng: -0.922852, lat: 47.886881 },
-  { lng: -0.769043, lat: 48.231991 },
-  { lng: 0.395508, lat: 48.268569 },
-  { lng: 0.604248, lat: 48.026672 },
-  { lng: 1.2854, lat: 47.982568 },
-  { lng: 1.318359, lat: 47.894248 },
-  { lng: 1.373291, lat: 47.879513 },
-  { lng: 1.384277, lat: 47.798397 },
-  { lng: 1.329346, lat: 47.754098 },
-  { lng: 1.329346, lat: 47.680183 },
-  { lng: 0.999756, lat: 47.635784 },
-  { lng: 0.86792, lat: 47.820532 },
-  { lng: 0.571289, lat: 47.820532 },
-  { lng: 0.439453, lat: 47.717154 },
-  { lng: 0.439453, lat: 47.61357 },
-  { lng: -0.571289, lat: 47.487513 },
-  { lng: -0.615234, lat: 47.680183 },
-  { lng: -0.812988, lat: 47.724545 },
-  { lng: -1.054688, lat: 47.680183 },
-  { lng: -1.219482, lat: 47.41322 }
-];
+// const poly1 = [
+//   { lng: -1.219482, lat: 47.41322 },
+//   { lng: -1.571045, lat: 47.457809 },
+//   { lng: -1.560059, lat: 47.739323 },
+//   { lng: -0.922852, lat: 47.886881 },
+//   { lng: -0.769043, lat: 48.231991 },
+//   { lng: 0.395508, lat: 48.268569 },
+//   { lng: 0.604248, lat: 48.026672 },
+//   { lng: 1.2854, lat: 47.982568 },
+//   { lng: 1.318359, lat: 47.894248 },
+//   { lng: 1.373291, lat: 47.879513 },
+//   { lng: 1.384277, lat: 47.798397 },
+//   { lng: 1.329346, lat: 47.754098 },
+//   { lng: 1.329346, lat: 47.680183 },
+//   { lng: 0.999756, lat: 47.635784 },
+//   { lng: 0.86792, lat: 47.820532 },
+//   { lng: 0.571289, lat: 47.820532 },
+//   { lng: 0.439453, lat: 47.717154 },
+//   { lng: 0.439453, lat: 47.61357 },
+//   { lng: -0.571289, lat: 47.487513 },
+//   { lng: -0.615234, lat: 47.680183 },
+//   { lng: -0.812988, lat: 47.724545 },
+//   { lng: -1.054688, lat: 47.680183 },
+//   { lng: -1.219482, lat: 47.41322 }
+// ];
 
 const tileProviders = [
   {
     name: "OpenStreetMap",
     visible: true,
-    attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    url: "	https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+    attribution:
+      '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    url:
+      "	https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
   }
 ];
 
@@ -131,7 +167,7 @@ export default {
   },
   data() {
     return {
-      center: [51.505, -0.09],
+      center: [48.866667, 2.333333],
       opacity: 0.6,
       token: "your token if using mapbox",
       mapOptions: {
@@ -139,7 +175,7 @@ export default {
         attributionControl: false,
         zoomSnap: true
       },
-      zoom: 13,
+      zoom: 8,
       minZoom: 1,
       maxZoom: 20,
       zoomPosition: "topleft",
@@ -152,68 +188,75 @@ export default {
       markers: [
         {
           id: "m1",
-          position: { lat: 51.505, lng: -0.09 },
-          tooltip: "tooltip for marker1",
-          draggable: true,
+          position: { lat: 49.4431, lng: 1.0993 },
+          tooltip: "LAN de Rouen",
+          draggable: false,
           visible: true,
           icon: icon.glyph({
             prefix: "",
-            glyph: "A"
+            glyph: "1"
           })
         },
         {
           id: "m2",
-          position: { lat: 51.8905, lng: -0.09 },
-          tooltip: "tooltip for marker2",
-          draggable: true,
-          visible: false
-        },
-        {
-          id: "m3",
-          position: { lat: 51.005, lng: -0.09 },
-          tooltip: "tooltip for marker3",
-          draggable: true,
-          visible: true
-        },
-        {
-          id: "m4",
-          position: { lat: 50.7605, lng: -0.09 },
-          tooltip: "tooltip for marker4",
-          draggable: true,
-          visible: false
+          position: { lat: 49.2534, lng: 4.033 },
+          tooltip: "LAN de Reims",
+          draggable: false,
+          visible: true,
+          icon: icon.glyph({
+            prefix: "",
+            glyph: "2"
+          })
         }
+        // {
+        //   id: "m2",
+        //   position: { lat: 51.8905, lng: -0.09 },
+        //   tooltip: "tooltip for marker2",
+        //   draggable: true,
+        //   visible: false
+        // },
+        // {
+        //   id: "m3",
+        //   position: { lat: 51.005, lng: -0.09 },
+        //   tooltip: "tooltip for marker3",
+        //   draggable: true,
+        //   visible: true
+        // },
+        // {
+        //   id: "m4",
+        //   position: { lat: 50.7605, lng: -0.09 },
+        //   tooltip: "tooltip for marker4",
+        //   draggable: true,
+        //   visible: false
+        // }
       ],
       polylines: [
-        {
-          id: "p1",
-          points: [
-            { lat: 37.772, lng: -122.214 },
-            { lat: 21.291, lng: -157.821 },
-            { lat: -18.142, lng: -181.569 },
-            { lat: -27.467, lng: -206.973 }
-          ],
-          visible: true
-        },
-        {
-          id: "p2",
-          points: [
-            [-73.91, 40.78],
-            [-87.62, 41.83],
-            [-96.72, 32.76]
-          ],
-          visible: true
-        }
+        // {
+        //   id: "p1",
+        //   points: [
+        //     { lat: 37.772, lng: -122.214 },
+        //     { lat: 21.291, lng: -157.821 },
+        //     { lat: -18.142, lng: -181.569 },
+        //     { lat: -27.467, lng: -206.973 }
+        //   ],
+        //   visible: true
+        // },
+        // {
+        //   id: "p2",
+        //   points: [[-73.91, 40.78], [-87.62, 41.83], [-96.72, 32.76]],
+        //   visible: true
+        // }
       ],
-      stuff: [
-        {
-          id: "s1",
-          markers: markers1,
-          polyline: { points: poly1, visible: true },
-          visible: true,
-          markersVisible: true
-        }
-      ],
-      bounds: latLngBounds({ lat: 51.476483373501964, lng: -0.14668464136775586 }, { lat: 51.52948330894063, lng: -0.019140238291583955 })
+      // stuff: [
+      //   {
+      //     id: "s1",
+      //     markers: markers1,
+      //     polyline: { points: poly1, visible: false },
+      //     visible: true,
+      //     markersVisible: true
+      //   }
+      // ],
+      bounds: latLngBounds({ lat: 48.2, lng: 4.5 }, { lat: 51, lng: 0 })
     };
   },
   methods: {
@@ -249,7 +292,7 @@ export default {
 body {
   margin: 0px;
 }
-#fdp {
+#mymap {
   position: relative;
   height: 100vh;
 }
